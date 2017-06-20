1e9fbfaa7c2a57013661d7da8a01353a2f48d55f
Pawan Sasanka Ammanamanchi <Shashi456@users.noreply.github.com>
2017-06-20 16:26:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
+import urllib
 import json
 import os
 
@@ -26,36 +20,28 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebHookResult(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
+ 
+ def makeWebhookResult(req):
     if req.get("result").get("action") != "HotelSearch":
         return {}
     re = req.get("result")
     parameters = result.get("parameters")
     city=parameters.get("geo-city")
-    city=str(city)
-    if city is none : 
-        return{}
-    city.lower()
-    res=makeWebhookresult(city)
-    return res
- 
- def makeWebhookResult(city):
     
     place={"barcelona"="guide/spain/barcelona/" ,
     "paris"="guide/france/paris"
     }
     
     main="https://www.alpharooms.com/"
-    search=place[city]
+    search=str(place[city])
     end="default.aspx?channel=AlphaRoomsUK"
 
     # print(json.dumps(item, indent=4))
